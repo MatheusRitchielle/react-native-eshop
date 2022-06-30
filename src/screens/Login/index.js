a83fce6b847f66395ccc1a7e96378961f2dc706a
MatheusRitchielle <matheusritchielle98@gmail.com>
2022-06-30 11:14:20 -0300

    assets

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -3,24 +3,24 @@ import { View } from "react-native";
 import { InnerText } from "../../components/CustomInput/styles";
 import { CustomText } from "../../components/CustomText/styles";
 import { CustomContainer } from "../../components/CustomContainer/styles";
-import { PrimaryButton,  SecondaryButton } from "../../components/CustomButton/styles";
+import { PrimaryButton, SecondaryButton } from "../../components/CustomButton/styles";
 import { CustomTitle } from "../../components/CustomTitle/styles";
 
 const Login = ({ navigation }) => {
   return (
     <CustomContainer>
       <CustomTitle>Login</CustomTitle>
-      <View style={{ marginTop: 20 }}/>
-        <InnerText placeholder="username" />
-        <InnerText placeholder="password" />
+      <View style={{ marginTop: 20 }} />
+      <InnerText placeholder="username" />
+      <InnerText placeholder="password" />
 
-        <SecondaryButton onPress={() => navigation.navigate("Recuperar Senha")}>
-          <CustomText style={{ color: "#646464" }}>Esqueci a senha</CustomText>
-        </SecondaryButton>
-        <View style={{ marginVertical: 50 }}>
-          <PrimaryButton onPress={() => navigation.navigate("Rotas")}>
-            <CustomText>Entrar</CustomText>
-          </PrimaryButton>
+      <SecondaryButton onPress={() => navigation.navigate("Recuperar Senha")}>
+        <CustomText style={{ color: "#646464" }}>Esqueci a senha</CustomText>
+      </SecondaryButton>
+      <View style={{ marginVertical: 50 }}>
+        <PrimaryButton onPress={() => navigation.navigate("Rotas")}>
+          <CustomText>Entrar</CustomText>
+        </PrimaryButton>
 
         <PrimaryButton onPress={() => navigation.navigate("Menu Principal")}>
           <CustomText>Login</CustomText>
