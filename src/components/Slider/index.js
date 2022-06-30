ff7af68f2234409e96184dcdc8d2438d3b280d08
Leonardo Chermaut <chermauts@hotmail.com>
2022-06-30 11:59:27 -0300

    implements carrousel

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,35 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   FlatList,
   SafeAreaView,
   StyleSheet,
   Image,
-  Dimensios,
+  Dimensions,
 } from "react-native";
 
-const { widht } = Dimensions.get("window");
+const { width } = Dimensions.get("window");
 const images = [
   {
     id: 1,
     url: "https://images.lojanike.com.br/1024x1024/produto/tenis-nike-af-11-masculino-DB2576-001-1-11627399691.jpg",
+  },
+  {
+    id: 2,
+    url: "https://images.lojanike.com.br/500x500/produto/tenis-nike-sportswear-air-max-terrascape-90-masculino-DH2973-001-4-41634137324.jpg",
   },
 ];
 
 const OnBoardingItem = ({ item }) => {
   return <Image source={{ uri: item.url }} style={styles.image} />;
 };
+
 const Slider = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
         data={images}
-        style={{ maxHeight: widht }}
+        style={{ maxHeight: width }}
         paddingEnable
         horizontal
+        onMomentumScrollEnd={(event) => {
+          setActiveIndex(parseInt(event.nativeEvent.contentOffset.x / width));
+        }}
+        scrollEventThrottle={16}
         showsHorizontalScrollIndicator={false}
-        keyExtractor={({ item }) => <OnBoardingItem item={item} />}
+        keyExtractor={({ item }) => String(item?.id)}
+        renderItem={({ item }) => <OnBoardingItem item={item} />}
       />
+      {images.length > 1 ? (
+        <View style={styles.dotsContainer}>
+          {images.map((_, i) => (
+            <View
+              style={[
+                styles.dot,
+                { backgroundColor: i === activeIndex ? "green" : "gray" },
+              ]}
+            />
+          ))}
+        </View>
+      ) : null}
     </SafeAreaView>
   );
 };
@@ -39,8 +62,19 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   image: {
-    widht,
-    height: widht,
+    width,
+    height: width,
+  },
+  dotsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 10,
+  },
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginHorizontal: 1,
   },
 });
 export default Slider;
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { View } from "react-native";
+
 import ContainerDinamico from "../../components/CustomContainer";
 import { CustomTitle } from "../../components/CustomTitle/styles";
 import { Ionicons } from "@expo/vector-icons";
+import Slider from "../../components/Slider";
 
 const Principal = ({ navigation }) => {
   return (
     <ContainerDinamico>
       <Ionicons name="home" size={24} color="black" />
       <CustomTitle>Pagina Principal</CustomTitle>
-      <View style={{ marginTop: 20 }}></View>
+      <Slider />
     </ContainerDinamico>
   );
 };
